6a61dbf96d0dd4e3dab362255a8a18140ed8789f
SegZWealth <41188367+SegZWealth@users.noreply.github.com>
2019-03-07 08:33:13 +0100

    CreateNewArray

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace interleave2arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] A = new string[] { "1", "2", "3" };
+            string[] B = new string[] { "A", "B", "C" };
+            int lengthA = A.Length;
+            int lengthB = B.Length;
+            string[] c = new string[lengthA + lengthB];
+
+            int counterA = 0;
+            int counterB = 0;
+
+            while (c.Length != (counterA + counterB))
+            {
+                if (A.Length - 1 >= counterA)
+                {
+                    c[counterA + counterB] = A[counterA];
+                    counterA++;
+                }
+
+                if (B.Length - 1 >= counterB)
+                {
+                    c[counterA + counterB] = B[counterB];
+                }
+                counterB++;
+            }
+
+            foreach (var item in c)
+            {
+                Console.Write(item + ",");
+            }
+
+
+            Console.ReadLine();
+
+        }
+
+       
+    }
+}
